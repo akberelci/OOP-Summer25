0cfb52a2cc439b4fda2692d13ce87ebbc5a71063
akberelci <akberelc10@gmail.com>
2025-03-11 11:22:01 +0100

    Update basic_data_structures.py

--- basic_data_structures.py
+++ basic_data_structures.py
@@ -1,1 +1,24 @@
+# Integer 
+age = 19  
+
+# Floating point 
+height = 1.85  
+
+# String 
+name = "Akber"  
+
+# List 
+numbers = [1, 2, 3]  
+
+# Dictionary 
+person = {"name": "Akber", "age": 30}  
+
+# Tuple 
+colors = ("red", "green")  
+
+# Set 
+fruits = {"apple", "banana"}  
+
+# Boolean 
+is_student = False  
 
