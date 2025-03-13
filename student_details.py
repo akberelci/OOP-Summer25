7b7a231ef282d07150f3bce78a7fa6090af29118
akberelci <akberelc10@gmail.com>
2025-03-13 10:31:13 +0100

    Update student_details.py

--- student_details.py
+++ student_details.py
@@ -3,4 +3,11 @@ last_name = "Elci"
 index_number = "35397"
 nationality = "Turkish"
 starting_date = "3/13/2025"
-courses = "Computer Science"
+courses = ["Mathematics","Computer Science"]
+
+print(f"First Name: {first_name}")
+print(f"Last Name: {last_name}")
+print(f"Index Number: {index_number}")
+print(f"Nationality: {nationality}")
+print(f"Starting Date: {starting_date}")
+print(f"Courses: {', '.join(courses)}")
