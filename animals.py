647010f8713c91040711a07aaa7e99563c33d713
akberelci <akberelc10@gmail.com>
2025-03-20 11:00:08 +0100

    animals.py

--- animals.py
+++ animals.py
@@ -7,3 +7,21 @@ animals = [
 ]
 
 print(animals)
+
+class Animal:
+    def __init__(self, name, group, number_of_legs, skills):
+        self.name = name
+        self.group = group
+        self.number_of_legs = number_of_legs
+        self.skills = skills
+
+animals = [
+    Animal("Dog", "Mammals", 4, ["Running", "Barking"]),
+    Animal("Bird", "Birds", 2, ["Flying"]),
+    Animal("Cat", "Mammals", 4, ["Jumping", "Climbing"]),
+    Animal("Cow", "Mammals", 4, ["Grazing", "Producing milk"]),
+    Animal("Snake", "Reptiles", 0, ["Slithering", "Hunting"]),
+]
+
+for animal in animals:
+    print(animal.name, animal.group, animal.number_of_legs, animal.skills)
