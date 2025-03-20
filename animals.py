7cff39cf739766fb3849956cae6cd458938a86fc
akberelci <akberelc10@gmail.com>
2025-03-20 10:17:35 +0100

    Update animals.py

--- animals.py
+++ animals.py
@@ -1,1 +1,9 @@
+animals = [
+    {"name": "Dog", "group": "Mammals", "number_of_legs": 4, "skills": ["Running", "Barking"]},
+    {"name": "Bird", "group": "Birds", "number_of_legs": 2, "skills": ["Flying"]},
+    {"name": "Cat", "group": "Mammals", "number_of_legs": 4, "skills": ["Jumping", "Climbing"]},
+    {"name": "Cow", "group": "Mammals", "number_of_legs": 4, "skills": ["Grazing", "Producing milk"]},
+    {"name": "Snake", "group": "Reptiles", "number_of_legs": 0, "skills": ["Slithering", "Hunting"]}
+]
 
+print(animals)
